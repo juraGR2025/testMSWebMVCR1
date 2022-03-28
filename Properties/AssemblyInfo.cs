e1e6e6a7dbbcf7434117adc87ab233e348da77fd
juraGR2025 <93720304+juraGR2025@users.noreply.github.com>
2022-03-28 22:33:21 +0300

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("58d618d4-20f2-49e8-9926-3e7de34739e7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Web.Mvc;
+using WebMVCR1.Controllers;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+            [TestMethod]
+            public void ActionExecutes_ReturnView()
+            {
+                MyController controller = new MyController();
+                var result = controller.Index();
+                Assert.IsNotNull(result);
+            }
+            [TestMethod]
+            public void IndexStringInViewData()
+            {
+                MyController controller = new MyController();
+                ViewResult result = controller.Index() as ViewResult;
+                Assert.AreEqual("Добрый день", result.ViewData["Greeting"]);
+            }
+    }
+}
