c693f474789003edaaa4a3a88f6810aa45a4f241
juraGR2025 <93720304+juraGR2025@users.noreply.github.com>
2022-03-28 22:38:30 +0300

    Add files via upload

---
+++ WebMVCR1/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMVCR1.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        public string Index(string hel)
+        {
+            int hour = DateTime.Now.Hour;
+            string Greeting = hour < 12 ? "Доброе утро" : "Добрый день" + ", " + hel;
+
+return Greeting;
+        }
+
+    }
+}
